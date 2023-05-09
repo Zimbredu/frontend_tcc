8bfbc92604895809c9ce11ae8507a22c7b6d0441
Eduardo <eduardo_zimbre@yahoo.com.br>
2023-05-09 19:24:57 -0300

    Principal

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript} from 'next/document';
+//import  Footer from 'next/document'
+
+export default function Document(){
+    return(
+        <Html>
+            <Head>
+
+            </Head>
+            <body>
+                <Main/>
+                <NextScript/>                
+            </body>
+            <footer>
+                <h1>rodapé</h1>
+                <NextScript/>
+            </footer>
+        </Html>
+       
+    )
+
+    
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from '../styles/home.module.scss';
+import logoImg from '../../public/desenho4.svg';
+
+import { Input } from "../components/ui/Input";
+
+
+export default function Home() {
+  return (
+   <>
+
+    <Head>
+      <title>Usuário - Faça seu login </title>
+    </Head>
+    <div className={styles.containerCenter}>
+     <Image src={logoImg} alt="Logo App"/>
+
+      <div className={styles.login}>
+          <form action="">
+          <Input
+              placeholder="Digite seu email"
+              type="text"
+            />
+
+            <Input
+              placeholder="Digite sua senha"
+              type="password"
+            />
+          
+          </form>
+      </div>
+    </div>
+
+   </>
+
+  )
+}
