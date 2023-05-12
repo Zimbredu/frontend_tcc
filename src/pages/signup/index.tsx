eeb1b08187c3479a174cad6ee463a96fb9ef35d3
Eduardo <eduardo_zimbre@yahoo.com.br>
2023-05-12 14:43:52 -0300

    Finalizada a tela de cadastro

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,26 +1,33 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from '../styles/home.module.scss';
-import logoImg from '../../public/desenho4.svg';
+import styles from '../../styles/home.module.scss';
+import logoImg from '../../../public/desenho4.svg';
 
-
-import { Input } from "../components/ui/Input";
-import { Button } from '../components/ui/Button';
+import { Input } from "../../components/ui/Input";
+import { Button } from '../../components/ui/Button';
 
 /* O import a seguir é para nevegação de página */
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Usuário - Faça seu login </title>
+        <title> Faça seu cadastro agora </title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo App" />
 
         <div className={styles.login}>
-          <form action="">
+          <h1>Criando sua conta</h1>
+
+
+          <form >
+          <Input
+              placeholder="Digite seu nome"
+              type="text"
+            />
+
             <Input
               placeholder="Digite seu email"
               type="text"
@@ -31,17 +38,15 @@ export default function Home() {
               type="password"
             />
 
-            <Button type='submit'
-              loading={false}
-            >
-              Acessar
+            <Button type='submit'loading={false}>
+              cadastrar
             </Button>
           </form>   
 
           {/* Precisei adicionar o atributo lagacyBehavior
           para a taga <a> funcionar dentro da tag <Link> */}        
-          <Link href='/signup' legacyBehavior>
-              <a className={styles.text}>Não possui uma conta? Cadastra-se</a>
+          <Link href='/' legacyBehavior>
+              <a className={styles.text}>Já possui uma conta? Faça o login!</a>
           </Link>
         </div>
       </div>
