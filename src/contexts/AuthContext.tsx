28dfcb2251eebf41cabedd9c1c29a99625ff9250
Eduardo <eduardo_zimbre@yahoo.com.br>
2023-05-17 15:36:35 -0300

    Instalando axios, nookies e jwt paras funcionalidade de login/logout.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,14 @@
 
-import { createContext, experimental_useOptimistic, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
+
+import { destroyCookie } from "nookies";
+import  Router from 'next/router';
 
 type AuthContextData = {
     user: UserProps;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>
+    signOut: () => void;
 }
 
 type UserProps = {
@@ -22,7 +26,18 @@ type AuthProviderProps = {
     children: ReactNode;
 }
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
+
+//Inicio funcionalidade de logout.
+export function signOut(){
+    try{
+        destroyCookie(undefined, '@nextauth.token');
+        Router.push('/')
+    }catch{
+        console.log('Erro ao deslogar.')
+    }
+}
+//Fim da funcionalidade de logout.
 
 export function AuthProvider({ children }: AuthProviderProps){ 
     const [user, setUser] = useState<UserProps>()
@@ -33,12 +48,12 @@ export function AuthProvider({ children }: AuthProviderProps){
     const isAuthenticated = !!user;
 
    async function signIn({email, password}: SignInProps){
-        console.log('DADOS PARA LOGAGR', experimental_useOptimistic);
+        console.log('DADOS PARA LOGAGR', email);
         console.log('SENHA', password);
     }
 
     return(
-        <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
+        <AuthContext.Provider value={{user, isAuthenticated, signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     )
